9c8044c271b5dd8c3266067e579bebd026e545d8
Andreas Bathen <a.bathen@gmx.de>
2012-02-13 14:15:20 +0100

    Remove default Rails page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,11 +6,11 @@ describe "StaticPages" do
   
   describe "Home page" do
     it "should have the h1 'Sample App'" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_selector('h1', :text => 'Sample App')
     end
     it "should have the title 'Home'" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_selector('title', 
       					:text => "#{base_title} | Home")
     end
@@ -18,11 +18,11 @@ describe "StaticPages" do
 
   describe "Help page" do
     it "should have the h1 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_selector('h1', :text => 'Help')
     end
     it "should have the title 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_selector('title', 
       					:text => "#{base_title} | Help")
     end
@@ -30,11 +30,11 @@ describe "StaticPages" do
 
   describe "About page" do
   	it "should have the h1 'About'" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_selector('h1', :text => 'About Us')
     end
     it "should have the title 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_selector('title', 
       					:text => "#{base_title} | About Us")
     end
@@ -42,11 +42,11 @@ describe "StaticPages" do
 
   describe "Contact page" do
   	it "should have the h1 'Contact'" do
-      visit '/static_pages/contact'
+      visit contact_path
       page.should have_selector('h1', :text => 'Contact')
     end
     it "should have the title 'Contact'" do
-      visit '/static_pages/contact'
+      visit contact_path
       page.should have_selector('title', 
       					:text => "#{base_title} | Contact")
     end
